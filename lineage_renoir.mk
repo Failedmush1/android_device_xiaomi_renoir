4f73d18d751f1ed44184d71e0279e9075f6bd05a
Murtaza <murtazakolachi7@gmail.com>
2025-06-03 09:46:43 +0500

    Renoir: Axion bringup

--- lineage_renoir.mk
+++ lineage_renoir.mk
@@ -10,6 +10,30 @@ $(call inherit-product, device/xiaomi/renoir/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+#Axion Bringup!
+
+AXION_CAMERA_REAR_INFO := 64,13,5
+
+AXION_CAMERA_FRONT_INFO := 20
+
+AXION_MAINTAINER := Failedmush
+
+AXION_PROCESSOR := Snapdragon_780g
+
+TARGET_ENABLE_BLUR := true
+
+AXION_CPU_PRIME_CORES := 7
+AXION_CPU_BIG_CORES := 4,5,6
+AXION_CPU_SMALL_CORES := 0,1,2,3
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.axion_cpu_prime=$(AXION_CPU_PRIME_CORES) \
+    persist.sys.axion_cpu_big=$(AXION_CPU_BIG_CORES) \
+    persist.sys.axion_cpu_small=$(AXION_CPU_SMALL_CORES)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
