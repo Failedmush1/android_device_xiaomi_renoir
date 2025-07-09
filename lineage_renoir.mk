89ecbeed3fa853353e0b4f61bf83d39b503ecf13
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-09 11:02:13 +0900

    Change model number

--- lineage_renoir.mk
+++ lineage_renoir.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := M2101K9G
+PRODUCT_MODEL := M2101K9R
 PRODUCT_NAME := lineage_renoir
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
