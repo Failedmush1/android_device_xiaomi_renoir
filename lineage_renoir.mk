6bc933bed00625cc3fe6d3b56caba4ce860b2e7f
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-19 18:33:42 +0900

    Renoir: welcome @Voltageos

--- lineage_renoir.mk
+++ lineage_renoir.mk
@@ -7,25 +7,16 @@
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GMS := true
-TARGET_USES_MINI_GAPPS := true
-BUILD_BCR := true
 
-ifeq ($(TARGET_USES_MINI_GAPPS),true)
-PRODUCT_PACKAGES += \
-    TurboAdapter
-endif
-TARGET_DISABLE_EPPE := true
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
+# Inherit some common Voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1920
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2101K9R
-PRODUCT_NAME := lineage_renoir
+PRODUCT_NAME := voltage_renoir
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="renoir_global-user 13 TKQ1.220829.002 V14.0.7.0.TKIMIXM release-keys" \
