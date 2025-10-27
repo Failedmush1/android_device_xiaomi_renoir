0e022fb19b570b1ce16f1fe901a85b1eb79e90eb
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-10-27 12:20:24 +0900

    Update lineage_renoir.mk

--- lineage_renoir.mk
+++ lineage_renoir.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/renoir/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Additions
+$(call inherit-product-if-exists, vendor/gapps/common.mk)
+
 -include vendor/lineage-priv/keys/keys.mk
 
 
