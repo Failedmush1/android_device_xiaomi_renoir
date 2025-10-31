3e4be7155400d0aa452bf88201a6a272f6ff2a31
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-10-31 21:21:16 +0900

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ read -p "Do you want to build with gapps support? (yes/no): " USER_INPUT
 if [[ "$USER_INPUT" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     echo "Gapps support enabled."
     echo "Cloning gapps source from crdroid gitlab..."
-    git clone --depth=1 https://gitlab.com/crdroidandroid/android-vendor-gapps-spes.git -b 13.0 vendor/gapps
+    git clone --depth=1 https://gitlab.com/Jabiyeff/android_vendor_gapps -b 13.0 vendor/gapps
 else
     echo "Gapps support disabled. Skipping ..."
     rm -rf vendor/gapps
