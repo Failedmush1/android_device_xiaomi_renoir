de71ff4248eb2c3e2e989edcf4f93c9f34fe2756
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-11-30 11:17:16 +0900

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,21 +1,17 @@
-# Gapps
-
-echo -e "${color}Setup Gapps ${end}"
-
-read -p "Do you want to build with gapps support? (yes/no): " USER_INPUT
-
 if [[ "$USER_INPUT" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-
-echo "Gapps support enabled."
-
-echo "Cloning gapps source from crdroid gitlab..."
-
-git clone --depth=1 https://gitlab.com/MindTheGapps/vendor_gapps -b tau vendor/gapps
-
+    echo "Gapps support enabled."
+    if [ -d "vendor/gapps" ]; then
+        rm -rf vendor/gapps
+    fi
+    echo "Cloning gapps source from crdroid gitlab..."
+    git clone --depth=1 https://gitlab.com/MindTheGapps/vendor_gapps -b tau vendor/gapps
+    
+    # Add Soong export for vendor/gapps
+    echo "PRODUCT_SOONG_NAMESPACES += vendor/gapps" >> device/*/AndroidProducts.mk || \
+    echo "PRODUCT_SOONG_NAMESPACES += vendor/gapps" >> vendor/lineage/config/crdroid.mk
+    
+    echo "GApps Soong namespace exported."
 else
-
-echo "Gapps support disabled. Skipping ..."
-
-rm -rf vendor/gapps
-
+    echo "Gapps support disabled. Skipping ..."
+    rm -rf vendor/gapps
 fi
