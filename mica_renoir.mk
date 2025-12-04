5e8bf9b4ee2f7adc2ffa7b2b0f9375d2eef86d2b
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-04 17:14:41 +0900

    Mica bringup

--- mica_renoir.mk
+++ mica_renoir.mk
@@ -7,12 +7,8 @@
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Additions
-$(call inherit-product-if-exists, vendor/gapps/common.mk)
-$(call inherit-product-if-exists, vendor/prebuilt-apps/config.mk)
+# Inherit some common mica stuff.
+$(call inherit-product, vendor/mica/config/common_full_phone.mk)
 
 -include vendor/lineage-priv/keys/keys.mk
 
@@ -22,7 +18,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2101K9R
-PRODUCT_NAME := lineage_renoir
+PRODUCT_NAME := mica_renoir
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="renoir_global-user 13 TKQ1.220829.002 V14.0.7.0.TKIMIXM release-keys" \
