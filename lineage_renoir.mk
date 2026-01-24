d662e7b09f687c18028607a93843c5e6a4fe8b6d
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-01-24 18:26:55 +0900

    LumineDroid upbringing
    Removed Axion-specific configurations and updated comments.

--- lineage_renoir.mk
+++ lineage_renoir.mk
@@ -7,24 +7,12 @@
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common LumineDroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+LUMINE_MAINTAINER := Failedmush
 
 -include vendor/lineage-priv/keys/keys.mk
 
-#Axion Bringup!
-AXION_CAMERA_REAR_INFO := 64,13,5
-AXION_CAMERA_FRONT_INFO := 20
-AXION_MAINTAINER := Failedmush
-AXION_PROCESSOR := Snapdragon_780g
-TARGET_ENABLE_BLUR := false
-AXION_CPU_PRIME_CORES := 7
-AXION_CPU_BIG_CORES := 4,5,6
-AXION_CPU_SMALL_CORES := 0,1,2,3
-PRODUCT_SYSTEM_PROPERTIES += \
-    persist.sys.axion_cpu_prime=$(AXION_CPU_PRIME_CORES) \
-    persist.sys.axion_cpu_big=$(AXION_CPU_BIG_CORES) \
-    persist.sys.axion_cpu_small=$(AXION_CPU_SMALL_CORES)
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
