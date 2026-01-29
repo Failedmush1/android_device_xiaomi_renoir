78f756086914083d1e5281c0540ed004cbfbe772
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-01-30 07:45:08 +0900

    renoir: Clover Bringups

--- clover_renoir.mk
+++ clover_renoir.mk
@@ -7,11 +7,12 @@
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Clover stuff.
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
--include vendor/gms/gms_pico.mk
-
+# Clover Kingdom
+CLOVER_BUILDTYPE := UNOFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
 
 -include vendor/lineage-priv/keys/keys.mk
 
@@ -19,7 +20,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2101K9R
-PRODUCT_NAME := lineage_renoir
+PRODUCT_NAME := clover_renoir
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="renoir_global-user 13 TKQ1.220829.002 V14.0.7.0.TKIMIXM release-keys" \
