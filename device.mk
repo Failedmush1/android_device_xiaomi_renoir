336f61b24a9054d2cfc046af88af4299008b977b
Failedmush <anytimewicked@gmail.com>
2026-05-25 11:39:30 +0000

    renoir: Add RRO for Secure NFC capability
    This modernizes the Secure NFC patch for Android 16 by using a
    Runtime Resource Overlay (RRO) targeting the NFC mainline module.
    This replaces the previous source patch for NfcService.java.
    
    Signed-off-by: Failedmush <anytimewicked@gmail.com>

--- device.mk
+++ device.mk
@@ -31,7 +31,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
-    ApertureOverlayRenoir
+    ApertureOverlayRenoir \
+    NfcOverlayRenoir
 
 PRODUCT_PACKAGES += \
     FrameworkOverlayRenoir \
