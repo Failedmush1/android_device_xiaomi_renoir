6620ae2e3d5d715a5e520624d4fe0fc64bb72ea0
Failedmush <anytimewicked@gmail.com>
2026-06-09 13:42:07 +0000

    renoir: Set SAKURA_MAINTAINER to Failedmush

--- lineage_renoir.mk
+++ lineage_renoir.mk
@@ -21,6 +21,7 @@ PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2101K9R
 PRODUCT_NAME := lineage_renoir
+SAKURA_MAINTAINER := Failedmush
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="renoir_global-user 13 TKQ1.220829.002 V14.0.7.0.TKIMIXM release-keys" \
