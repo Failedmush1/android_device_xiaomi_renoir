76c18e1b59bda14719939da9b7230a0b4950ae90
Failedmush <anytimewicked@gmail.com>
2026-09-04 11:26:03 +0000

    renoir: Add vendorsetup patch to add NFC icon to statusbar

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,3 +10,14 @@ if [ ! -f frameworks/base/.patch_applied_4g_icon ]; then
 else
     echo "4G icon patch already applied to frameworks/base."
 fi
+
+# Apply NFC Statusbar Icon patch to frameworks/base
+if [ ! -f frameworks/base/.patch_applied_nfc_icon ]; then
+    echo "Applying NFC Statusbar Icon patch to frameworks/base..."
+    cd frameworks/base
+    git apply ../../device/xiaomi/renoir/patches/0002-SystemUI-Add-status-bar-NFC-icon.patch
+    touch .patch_applied_nfc_icon
+    cd ../..
+else
+    echo "NFC Statusbar Icon patch already applied to frameworks/base."
+fi
