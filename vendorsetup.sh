cfd41f9e2f90960ee3fde1db97c6708582272d62
Failedmush <anytimewicked@gmail.com>
2026-09-05 05:38:49 +0000

    renoir: Remove default 4G icon patch (now handled by toggle)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Apply 4G Icon patch to frameworks/base
-if [ ! -f frameworks/base/.patch_applied_4g_icon ]; then
-    echo "Applying 4G icon patch to frameworks/base..."
-    cd frameworks/base
-    git apply ../../device/xiaomi/renoir/patches/0001-telephony-Default-to-showing-4G-icon-instead-of-LTE.patch
-    touch .patch_applied_4g_icon
-    cd ../..
-else
-    echo "4G icon patch already applied to frameworks/base."
-fi
-
 # Apply NFC Statusbar Icon patch to frameworks/base
 if [ ! -f frameworks/base/.patch_applied_nfc_icon ]; then
     echo "Applying NFC Statusbar Icon patch to frameworks/base..."
